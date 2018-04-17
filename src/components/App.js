44239c33b3cc38f91172765bd8aa4b7097b1e0bd
S.j. Sakib <sjsakib.bd@gmail.com>
2018-04-17 16:09:39 +0600

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import '../css/index.css';
 import GameContainer from '../containers/GameContainer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class App extends Component {
-	render() {
-		return <GameContainer />;
-	}
-}
+
+const Game = () => (
+  <div className="game-ui">
+    <GameContainer />
+    <div className="ui-link"><Link to='/'>Go Back</Link></div>
+  </div>
+)
+
+
+const Home = () => (
+  <div className="home-ui">
+    <div className="ui-link"><Link to="/game">Go To Game</Link></div>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/game" component={Game}/>
+    </div>
+  </Router>
+)
 
 export default App;

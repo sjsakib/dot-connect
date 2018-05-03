0229cbc8620b28abdeba3956343efa918de2926a
sjsakib <sjsakib.bd@gmail.com>
2018-05-03 16:11:21 +0600

    changed siteUrl

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 const apiUrl = 'https://dot-connect-server.now.sh'
-const siteUrl = 'http://dot-connect.netlify.com'
+const siteUrl = 'https://dot-connect.sjsakib.me'
 
 export {
   apiUrl,
   siteUrl,
 }
-

1af21bdff3f8eb8cc7e5950877c75e3275aea0eb
me-shaon <shaon.cse81@gmail.com>
2018-05-07 19:01:58 +0600

    Menu refactored

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import GameContainer from '../containers/GameContainer';
 import GameForm from '../components/GameForm';
 import JoinForm from '../components/JoinForm';
-import Menu from '../components/Menu';
+import Home from '../components/Home';
 import Rules from '../components/Rule';
 import ClientSocket from '../utilities/ClientSocket';
 
@@ -21,7 +21,7 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <div>
-                    <Route exact path="/" component={Menu} />
+                    <Route exact path="/" component={Home} />
                     <Route
                         exact
                         path="/game/:gameId/play"
